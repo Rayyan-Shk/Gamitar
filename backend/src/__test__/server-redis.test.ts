e1ad2ab201ee4455393bcf267c61212e36242a91
Rayyan-Shk <rayyanshaikhassociates@gmail.com>
2025-01-18 10:50:05 +0530

    Update: Test files

--- backend/src/__test__/server-redis.test.ts
+++ backend/src/__test__/server-redis.test.ts
@@ -1,20 +1,18 @@
-// server.test.ts
-
 import Redis from 'ioredis';
 import { io } from 'socket.io-client';
 
 describe('Redis and Socket Connectivity', () => {
   
-  // Test Redis connection
+  
   it('should connect to Redis server successfully', async () => {
     const redis = new Redis({
-      host: 'localhost', // or use your Redis host
-      port: 6379, // default Redis port
+      host: 'localhost',
+      port: 6379, //default Redis port
     });
 
     await redis.ping()
       .then((response) => {
-        expect(response).toBe('PONG'); // Expect PONG response from Redis
+        expect(response).toBe('PONG');
         redis.disconnect();
       })
       .catch((error) => {
@@ -23,19 +21,19 @@ describe('Redis and Socket Connectivity', () => {
       });
   });
 
-  // Test Socket.io connection
+  
   it('should connect to WebSocket server successfully', (done) => {
-    const socket = io('http://localhost:3002'); // Adjust to your socket server URL
+    const socket = io('http://localhost:3002');
 
     socket.on('connect', () => {
-      expect(socket.connected).toBe(true); // Check that socket is connected
+      expect(socket.connected).toBe(true); //Check that socket is connected
       socket.disconnect();
       done();
     });
 
     socket.on('connect_error', (error) => {
       console.error('Socket connection error:', error);
-      done(error); // Pass error to Jest
+      done(error); 
     });
   });
 });
